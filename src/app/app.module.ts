1a11fc1ce56024d9bd199fa73169e710b52ed58b
Yone <yone.work@gmail.com>
2018-03-02 20:56:46 +0200

    i

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+import { WeatherService } from './services/weather.service';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { WeeklyWeatherComponent } from './components/weekly-weather/weekly-weather.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeeklyWeatherComponent,
+    HomePageComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
